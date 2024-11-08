3b20637dcff237e63572b87ccdae5aed68750a23
Émery Freitas Moriconi <emerymoriconi09@gmail.com>
2024-11-08 13:41:54 -0300

    Iniciando o nav e home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import './App.css';
+import perfil from'./perfil.jpg';
+// para executar, colocar no terminal 'npm start'
 
-function App() {
+// um componente React é uma função JavaScript que retorna um elemento HTML (JSX)
+// o elemento HTML é o que será renderizado na tela
+// o componente App é o componente principal da aplicação
+// ele é renderizado no arquivo index.js
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Bem-vindo ao Meu Portfólio</h1>
-        <p>Este é o meu portfólio feito com React!</p>
+    <div className="home">
+      <header className="home-header">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About me</a>
+            </li>
+            <li>
+              <a href="#">Skills</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+
+      <main>
+        <section className='hero'>
+          <img className='profile-pic' src={perfil} alt='Foto de perfil' />
+          <h1>Olá, eu sou a Émery</h1>
+          <p>Desenvolvedora, apaixonada por tecnologia!</p>
+        </section>
+      </main>
     </div>
   );
 }
 
-export default App;
+export default Home;
